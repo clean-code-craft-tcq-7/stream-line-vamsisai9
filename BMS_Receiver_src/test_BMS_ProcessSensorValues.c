e9dd875769a083b387ea22a1d01b3bf639d50ad4
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-02-28 20:38:55 +0530

    Update test cases skeletons for receiver and data processor

---
+++ BMS_Receiver_src/test_BMS_ProcessSensorValues.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "BMS_ProcessSensorValues.h"
+
+void testMovingAverage()
+{
+
+}
+
+void testMinMaxFinder()
+{
+
+}
+
+void testSensorDataProcessor()
+{
+
+}
+
+void testProcessSensorValues()
+{
+    testMovingAverage();
+    testMinMaxFinder();
+    testSensorDataProcessor();
+}
--- BMS_Receiver_src/test_BMS_receiver.c
+++ BMS_Receiver_src/test_BMS_receiver.c
@@ -3,13 +3,26 @@
 #include <string.h>
 #include <assert.h>
 
+#include "BMS_Receiver.h"
 #include "test_BMS_Operations.h"
+#include "test_BMS_ProcessSensorValues.h"
+#include "test_BMS_ReadConsole.h"
+#include "BMS_IO_Mock.h"
 
+void testBmsReceiver()
+{
+
+}
 
 void test_BMS_Receiver()
 {
     printf("Running test cases for BMS sensor stream sender module...\n");
+    
     testBMSOperations();
-    printf("All tests are successful\n");
+    testReadHeader();
+    testReadSamples();
+    testProcessSensorValues();
+    testBmsReceiver();
 
+    printf("All tests are successful\n");
 }
