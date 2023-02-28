d7ee3f5f5f60128b387a0e42a415b334c1230669
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-02-28 20:41:44 +0530

    Integrate console reader, data processor and IO module as reciever

--- BMS_Receiver_src/BMS_Receiver.c
+++ BMS_Receiver_src/BMS_Receiver.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+#include "BMS_ReadConsole.h"
+#include "BMS_Receiver.h"
+#include "BMS_ProcessSensorValues.h"
+#include "BMS_Receiver_IO.h"
+
+bmsInputFuncPtr bmsInputFunc = getInputFromConsole;
+bmsOutputFuncPtr bmsOutputFunc = printReceiverOutput;
+
+void setStreamIoFunc(bmsInputFuncPtr inputFuncPtr, bmsOutputFuncPtr outputFuncPtr)
+{
+    bmsInputFunc = inputFuncPtr;
+    bmsOutputFunc = outputFuncPtr;
+}
+
 void receiveStreamData()
 {
+    receiverDataSet sensorDataSet[MAX_SENSORS_SUPPORTED];
+    
+    int count = readHeader(sensorDataSet, bmsInputFunc);
+    readSamples(sensorDataSet, bmsInputFunc);
+    
+    processSensorData(sensorDataSet, count, bmsOutputFunc);
 }
